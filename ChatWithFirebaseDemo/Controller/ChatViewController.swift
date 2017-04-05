d658155251b61a34f99328ad3e4ca9e741c7ab66
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-04-05 13:40:54 +0700

    - Create `ChatViewController`.

---
+++ ChatWithFirebaseDemo/Controller/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  ChatWithFirebaseDemo
+//
+//  Created by Kittitat Rodphotong on 4/5/2560 BE.
+//  Copyright © 2560 FameSprinter. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
