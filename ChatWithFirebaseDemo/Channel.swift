7a610f717e2f0cdcb40c3f7d2b3c9b3134111cea
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-04-05 11:08:05 +0700

    - Create model `Channel`.

---
+++ ChatWithFirebaseDemo/Channel.swift
@@ -0,0 +1,19 @@
+//
+//  Channel.swift
+//  ChatWithFirebaseDemo
+//
+//  Created by Kittitat Rodphotong on 4/5/2560 BE.
+//  Copyright © 2560 FameSprinter. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    var id: String
+    var name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
